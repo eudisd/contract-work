47ed8804d6bc9ab3aaf8874c705bf3a5d42108a6
Eudis Duran <eudisd@gmail.com>
2012-05-29 13:33:36 -0400

    Made it update first, depending on the default values

--- contract-4/js/running_total_main.js
+++ contract-4/js/running_total_main.js
@@ -5,6 +5,13 @@ $(document).ready(function($){
     org.running.close_date_1 = org.util.get_date($("#close_date_1").val());
     org.running.close_date_2 = org.util.get_date($("#close_date_2").val());
     
+    // First, check the default values, and update accordingly
+    var select_1 = $(this).find("option:selected").val(),
+        select_2 = $("select[name='select1_2']").find("option:selected").val();
+    
+    org.running.logic(select_1, select_2);
+    org.running.update_view($, $("#mytotal"));
+    
     // Update if the select drop-down is changed
     $("select[name='select1_1']").live('change', function(){
         var select_1 = $(this).find("option:selected").val(),

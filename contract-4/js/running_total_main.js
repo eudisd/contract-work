91d54672ac234994f094e1a44fb1ed41590538d8
Eudis Duran <eudisd@gmail.com>
2012-05-29 13:45:51 -0400

    Various changes to the new entryform

--- contract-4/js/running_total_main.js
+++ contract-4/js/running_total_main.js
@@ -12,23 +12,23 @@ $(document).ready(function($){
     org.running.logic(select_1, select_2);
     org.running.update_view($, $("#mytotal"));
     
-    // Update if the select drop-down is changed
-    $("select[name='select1_1']").live('change', function(){
-        var select_1 = $(this).find("option:selected").val(),
-            select_2 = $("select[name='select1_2']").find("option:selected").val();
-        
-        org.running.logic(select_1, select_2);
-        org.running.update_view($, $("#mytotal"));
-    });
-    
-    // Update if the radio button is changed
-    $("select[name='select1_2']").live('change', function(){
-        var select_1 = $("select[name='select1_2']").find("option:selected").val(),
-            select_2 = $(this).find("option:selected").val();
-        
-        org.running.logic(select_1, select_2);
-        org.running.update_view($, $("#mytotal"));
-    });
+    // // Update if the select drop-down is changed
+    //     $("select[name='select1_1']").live('change', function(){
+    //         var select_1 = $(this).find("option:selected").val(),
+    //             select_2 = $("select[name='select1_2']").find("option:selected").val();
+    //         
+    //         org.running.logic(select_1, select_2);
+    //         org.running.update_view($, $("#mytotal"));
+    //     });
+    //     
+    //     // Update if the radio button is changed
+    //     $("select[name='select1_2']").live('change', function(){
+    //         var select_1 = $("select[name='select1_2']").find("option:selected").val(),
+    //             select_2 = $(this).find("option:selected").val();
+    //         
+    //         org.running.logic(select_1, select_2);
+    //         org.running.update_view($, $("#mytotal"));
+    //     });
     
     var origin_y = parseInt($("#runningtotal").css("top"), 10);
 
